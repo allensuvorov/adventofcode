8f106d1b6e911f9e48902b1d940151753998303e
Allen Suvorov <allen.suvorov@gmail.com>
2024-12-04 21:31:24 -0800

    2024 dec day 4 - solve part two

--- 2024/day/4/main.go
+++ 2024/day/4/main.go
@@ -132,6 +132,87 @@ func countXmas() int {
 	return xmasCount
 }
 
+func enqueueCross(q []byte, b byte) []byte {
+	q = append(q, b)
+	if len(q) > 3 {
+		q = q[1:]
+	}
+	return q
+}
+
+func isMasLeft(q []byte) bool {
+	return string(q) == "MAS" || string(q) == "SAM"
+}
+
+func isMasRight(matrix [][]byte, r, c int) bool {
+	if matrix[r-1][c+1] == 'S' && matrix[r+1][c-1] == 'M' {
+		return true
+	}
+	if matrix[r-1][c+1] == 'M' && matrix[r+1][c-1] == 'S' {
+		return true
+	}
+	return false
+}
+func countDiagonalCross(matrix [][]byte) int {
+	xmasCount := 0
+	rows, cols := len(matrix), len(matrix[0])
+
+	// diagonal count left '\'
+	for r1 := rows - 1; r1 >= 0; r1-- {
+		q := []byte{}
+		c := 0
+		for r := r1; r < rows; r++ {
+			q = enqueueCross(q, matrix[r][c])
+			if isMasLeft(q) && isMasRight(matrix, r-1, c-1) {
+				xmasCount++
+			}
+			c++
+		}
+	}
+
+	// diagonal count top '\'
+	for c1 := 1; c1 < cols; c1++ {
+		q := []byte{}
+		r := 0
+		for c := c1; c < cols; c++ {
+			q = enqueueCross(q, matrix[r][c])
+			if isMasLeft(q) && isMasRight(matrix, r-1, c-1) {
+				xmasCount++
+			}
+			r++
+		}
+	}
+
+	return xmasCount
+}
+
+func countXmasCross(path string) int {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var xmasCount int
+
+	matrix := make([][]byte, 0, 140)
+	for scanner.Scan() {
+		line := scanner.Text()
+		matrix = append(matrix, []byte(line))
+	}
+
+	xmasCount += countDiagonalCross(matrix)
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return xmasCount
+}
+
 func main() {
-	fmt.Println(countXmas())
+	// fmt.Println(countXmas())
+	fmt.Println(countXmasCross("input.txt"))
+	fmt.Println(countXmasCross("input1.txt"))
 }
