1c545e16431f287b73b5af1b03f58610b9173a87
Allen Suvorov <allen.suvorov@gmail.com>
2024-12-04 19:27:42 -0800

    2024 dec day 4 - extracted countXmas func

--- 2024/day/4/main.go
+++ 2024/day/4/main.go
@@ -24,42 +24,40 @@ func main() {
 	}
 
 	rows, cols := len(matrix), len(matrix[0])
+	q := []byte{}
+
+	countXmas := func(r, c int) {
+		q = append(q, matrix[r][c])
+		if len(q) > 4 {
+			q = q[1:]
+		}
+		if string(q) == "XMAS" || string(q) == "SAMX" {
+			xmasCount++
+		}
+	}
 
 	// horisontal search
 	for r := range rows {
-		for c := 0; c < cols-5; c++ {
-			if string(matrix[r][c:c+4]) == "XMAS" || string(matrix[r][c:c+4]) == "SAMX" {
-				xmasCount++
-			}
+		q = []byte{}
+		for c := 0; c < cols; c++ {
+			countXmas(r, c)
 		}
 	}
 
 	// vertical search
-	q := []byte{}
 	for c := range cols {
+		q = []byte{}
 		for r := 0; r < rows; r++ {
-			q = append(q, matrix[r][c])
-			if len(q) > 4 {
-				q = q[1:]
-			}
-			if string(q) == "XMAS" || string(q) == "SAMX" {
-				xmasCount++
-			}
+			countXmas(r, c)
 		}
 	}
 
 	// diagonal count
-	q = []byte{}
 	for r1 := rows - 1; r1 >= 0; r1-- {
+		q = []byte{}
 		c := 0
 		for r := r1; r < rows; r++ {
-			q = append(q, matrix[r][c])
-			if len(q) > 4 {
-				q = q[1:]
-			}
-			if string(q) == "XMAS" || string(q) == "SAMX" {
-				xmasCount++
-			}
+			countXmas(r, c)
 			c++
 		}
 	}
